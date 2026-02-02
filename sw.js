04fd3cafd972a5ae010f3d1c8824be06e8c70f0b
SmArt-4 <kurbatov.artem@gmail.com>
2026-02-02 16:28:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'frodo-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './frodo.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 1. Install Event: Cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Fetch Event: Serve from Cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
